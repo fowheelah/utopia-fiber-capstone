0f6c819e1d518b3788fbd6bad477cfb70f906b8b
ProGenji25 <jordan.golden1234@yahoo.com>
2021-11-01 17:45:38 -0600

    assignment file added

---
+++ golde3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import json
+import requests
+import sys
+import datetime;
+
+url = 'https://api.purpleair.com/v1/sensors?fields=name&location_type=0&nwlng=-112.4&nwlat=41.74&selng=-111.55&selat=39.98'
+
+headers = {
+	'X-API-Key' : 'A27D8E70-260B-11EC-BAD6-42010A800017'
+	}
+	
+r = requests.get(url, headers = headers)
+
+files = r.json()
+
+date = datetime.datetime.now()
+
+datestring = "datafiles/" + date.strftime("%m")
+datestring += "-" + date.strftime("%d") + "-" + date.strftime("%Y") + "-" + date.strftime("%X")
+datestring += ".txt"
+
+with open(datestring, "w") as f:
+	print(files, file=f)
