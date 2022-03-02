b93c1969f68f25731a7103889bbfd134fb3bcd7e
Jarom Ortiz <aceinargentina@gmail.com>
2022-03-01 20:26:46 -0700

    Map integrated

--- frontend/vue-smartmap/src/main.js
+++ frontend/vue-smartmap/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 const app = createApp(App)
 
 app.use(router)
--- frontend/vue-smartmap/src/views/HomeView.vue
+++ frontend/vue-smartmap/src/views/HomeView.vue
@@ -1,10 +1,63 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+<script>
+/* eslint-disable no-undef */
+import { computed, ref, onMounted } from "vue";
+import { useGeolocation } from "../useGeolocation";
+import { Loader } from "@googlemaps/js-api-loader";
+import sensorData from "../data/sensors.json";
+
+const GOOGLE_MAPS_API_KEY = "AIzaSyDadMlYb0afBDeIzmUHunxH93BHubVu0wU";
+
+export default {
+  name: "App",
+  setup() {
+    const contentString = "Sample";
+    const { coords } = useGeolocation();
+    const currPos = computed(() => ({
+      lat: coords.value.latitude,
+      lng: coords.value.longitude,
+    }));
+    const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY });
+    const mapDiv = ref(null);
+    onMounted(async () => {
+      //sensorInfo = await fetch(url);
+      await loader.load();
+      const map = new google.maps.Map(mapDiv.value, {
+        center: currPos.value,
+        zoom: 8,
+      });
+      const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+      });
+      for (let index = 0; index < sensorData.data.length; index++) {
+        const marker = new google.maps.Marker({
+          position: {
+            lat: sensorData.data[index].latitude,
+            lng: sensorData.data[index].longitude,
+          },
+          map: map,
+        });
+        marker.addListener("click", () => {
+          infowindow.open({
+            anchor: marker,
+            map,
+            shouldFocus: false,
+          });
+        });
+      }
+    });
+
+    return { currPos, mapDiv };
+  },
+};
 </script>
 
 <template>
-  <main>
-    <h1>This is the Map page</h1>
-    <!-- <TheWelcome /> -->
-  </main>
+  <!-- <div class="d-flex text-center" style="height: 8vh">
+    <div class="m-auto">
+      <h4>Your Position</h4>
+      Latitude: {{ currPos.lat.toFixed(2) }}, Longitude:
+      {{ currPos.lng.toFixed(2) }}
+    </div>
+  </div> -->
+  <div ref="mapDiv" style="width: 100vw; height: 100vh" />
 </template>
