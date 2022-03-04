bce171770b57a26a7d4919708279d34f4748e5ed
Jarom Ortiz <aceinargentina@gmail.com>
2022-03-03 17:54:18 -0700

    Markers added to map

--- frontend/vue-smartmap/src/views/HomeView.vue
+++ frontend/vue-smartmap/src/views/HomeView.vue
@@ -5,7 +5,29 @@ import { useGeolocation } from "../useGeolocation";
 import { Loader } from "@googlemaps/js-api-loader";
 import sensorData from "../data/sensors.json";
 
-const GOOGLE_MAPS_API_KEY = "";
+const GOOGLE_MAPS_API_KEY = "AIzaSyDxcOcU0EH6pLvwESsB6T3pFo0uFkWFohM";
+const iconBase = "/src/assets/circle";
+const icons = {
+  green: {
+    icon: iconBase + "5.png",
+  },
+  yellow: {
+    icon: iconBase + "4.png",
+  },
+  orange: {
+    icon: iconBase + "3.png",
+  },
+  red: {
+    icon: iconBase + "2.png",
+  },
+  purple: {
+    icon: iconBase + "1.png",
+  },
+  magenta: {
+    icon: iconBase + "0.png",
+  },
+};
+let color = ["green", "yellow", "orange", "red", "purple", "magenta"];
 
 export default {
   name: "App",
@@ -25,7 +47,27 @@ export default {
         zoom: 8,
       });
       for (let index = 0; index < sensorData.data.length; index++) {
-        const contentString = "<ul style=\"list-style-type:none;\"><li><h2>Name: " + sensorData.data[index].name + "<h2></li><li><h3>PM 2.5: " + sensorData.data[index].pm25 + "<h3></li>";
+        const pm25 = sensorData.data[index].pm25;
+        let rating = color[0];
+        if (pm25 >= 0 && pm25 <= 12) {
+          rating = color[0];
+        } else if (pm25 > 12 && pm25 <= 35) {
+          rating = color[1];
+        } else if (pm25 > 35 && pm25 <= 55) {
+          rating = color[2];
+        } else if (pm25 > 55 && pm25 <= 150) {
+          rating = color[3];
+        } else if (pm25 > 150 && pm25 <= 250) {
+          rating = color[4];
+        } else if (pm25 > 250) {
+          rating = color[5];
+        }
+        const contentString =
+          '<ul style="list-style-type:none;"><li><h2>Name: ' +
+          sensorData.data[index].name +
+          "<h2></li><li><h3>PM 2.5: " +
+          pm25 +
+          "<h3></li>";
         const infowindow = new google.maps.InfoWindow({
           content: contentString,
         });
@@ -35,6 +77,7 @@ export default {
             lng: sensorData.data[index].longitude,
           },
           map: map,
+          icon: icons[rating].icon,
         });
         marker.addListener("click", () => {
           infowindow.open({
@@ -45,7 +88,6 @@ export default {
         });
       }
     });
-
     return { currPos, mapDiv };
   },
 };
