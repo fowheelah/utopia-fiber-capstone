33efaeb91e26d672dfe10b93f2af90dbe78808ef
Jarom Ortiz <aceinargentina@gmail.com>
2022-03-15 17:26:50 -0600

    Working circle icons and labels. Need color & text

--- frontend/vue-smartmap/src/views/HomeView.vue
+++ frontend/vue-smartmap/src/views/HomeView.vue
@@ -4,27 +4,34 @@ import { computed, ref, onMounted } from "vue";
 import { useGeolocation } from "../useGeolocation";
 import { Loader } from "@googlemaps/js-api-loader";
 import sensorData from "../data/sensors.json";
+import circle from "@/components/icons/circle.svg";
 
 const GOOGLE_MAPS_API_KEY = "AIzaSyDxcOcU0EH6pLvwESsB6T3pFo0uFkWFohM";
 const iconBase = "/src/assets/circle";
 const icons = {
   green: {
     icon: iconBase + "5.png",
+    label: "",
   },
   yellow: {
     icon: iconBase + "4.png",
+    label: "",
   },
   orange: {
     icon: iconBase + "3.png",
+    label: "",
   },
   red: {
     icon: iconBase + "2.png",
+    label: "",
   },
   purple: {
     icon: iconBase + "1.png",
+    label: "",
   },
   magenta: {
     icon: iconBase + "0.png",
+    label: "",
   },
 };
 let color = ["green", "yellow", "orange", "red", "purple", "magenta"];
@@ -44,13 +51,14 @@ export default {
       await loader.load();
       const map = new google.maps.Map(mapDiv.value, {
         center: currPos.value,
-        zoom: 8,
+        zoom: 12,
       });
       for (let index = 0; index < sensorData.data.length; index++) {
         const pm25 = sensorData.data[index].pm25;
+        let pm25_str = toString(pm25);
         let rating = color[0];
         if (pm25 >= 0 && pm25 <= 12) {
-          rating = color[0];
+          circle
         } else if (pm25 > 12 && pm25 <= 35) {
           rating = color[1];
         } else if (pm25 > 35 && pm25 <= 55) {
@@ -76,8 +84,9 @@ export default {
             lat: sensorData.data[index].latitude,
             lng: sensorData.data[index].longitude,
           },
+          label: "sample",
           map: map,
-          icon: icons[rating].icon,
+          icon: circle,
         });
         marker.addListener("click", () => {
           infowindow.open({
@@ -94,12 +103,5 @@ export default {
 </script>
 
 <template>
-  <!-- <div class="d-flex text-center" style="height: 8vh">
-    <div class="m-auto">
-      <h4>Your Position</h4>
-      Latitude: {{ currPos.lat.toFixed(2) }}, Longitude:
-      {{ currPos.lng.toFixed(2) }}
-    </div>
-  </div> -->
   <div ref="mapDiv" style="width: 100vw; height: 100vh" />
 </template>
