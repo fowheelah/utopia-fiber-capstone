c71fa9572ffb12d6035d757d82d0c509178dccaf
Jarom Ortiz <aceinargentina@gmail.com>
2022-03-22 20:11:08 -0600

    Markers complete. Styling needed.

--- frontend/vue-smartmap/src/views/HomeView.vue
+++ frontend/vue-smartmap/src/views/HomeView.vue
@@ -1,40 +1,18 @@
 <script>
 /* eslint-disable no-undef */
-import { computed, ref, onMounted } from "vue";
+import { computed, ref, onMounted, } from "vue";
 import { useGeolocation } from "../useGeolocation";
 import { Loader } from "@googlemaps/js-api-loader";
 import sensorData from "../data/sensors.json";
-import circle from "@/components/icons/circle.svg";
+import GreenIcon from '../components/icons/green_circle.svg'
+import YellowIcon from '../components/icons/yellow_circle.svg'
+import OrangeIcon from '../components/icons/orange_circle.svg'
+import RedIcon from '../components/icons/red_circle.svg'
+import PurpleIcon from '../components/icons/purple_circle.svg'
+import MagentaIcon from '../components/icons/magenta_circle.svg'
+import circle from '../components/icons/circle.svg'
 
-const GOOGLE_MAPS_API_KEY = "AIzaSyDxcOcU0EH6pLvwESsB6T3pFo0uFkWFohM";
-const iconBase = "/src/assets/circle";
-const icons = {
-  green: {
-    icon: iconBase + "5.png",
-    label: "",
-  },
-  yellow: {
-    icon: iconBase + "4.png",
-    label: "",
-  },
-  orange: {
-    icon: iconBase + "3.png",
-    label: "",
-  },
-  red: {
-    icon: iconBase + "2.png",
-    label: "",
-  },
-  purple: {
-    icon: iconBase + "1.png",
-    label: "",
-  },
-  magenta: {
-    icon: iconBase + "0.png",
-    label: "",
-  },
-};
-let color = ["green", "yellow", "orange", "red", "purple", "magenta"];
+const GOOGLE_MAPS_API_KEY = "AIzaSyB9Bz6Wi3ZJKSCbw6WzE61Bh6u1HJgTTiA";
 
 export default {
   name: "App",
@@ -54,7 +32,28 @@ export default {
         zoom: 12,
       });
       for (let index = 0; index < sensorData.data.length; index++) {
-        const contentString = "<ul style=\"list-style-type:none;\"><li><h2>Name: " + sensorData.data[index].name + "<h2></li><li><h3>PM 2.5: " + sensorData.data[index].pm25 + "<h3></li>";
+        let value = sensorData.data[index].pm25;
+        let icon = circle;
+        let label = value.toString();
+        if (value >= 0.0 && value <= 12.0) {
+          icon = GreenIcon;
+        } else if (value > 12.0 && value <= 35.4) {
+          icon = YellowIcon;
+        } else if (value > 35.4 && value <= 55.4) {
+          icon = OrangeIcon;
+        } else if (value > 55.4 && value <= 150.4) {
+          icon = RedIcon;
+        } else if (value > 150.4 && value <= 250.4) {
+          icon = PurpleIcon;
+        } else if (value > 250.4 && value <= 500.4) {
+          icon = MagentaIcon;
+        }
+        const contentString =
+          '<ul style="list-style-type:none;"><li><h2>Name: ' +
+          sensorData.data[index].name +
+          "<h2></li><li><h3>PM 2.5: " +
+          value +
+          "<h3></li>";
         const infowindow = new google.maps.InfoWindow({
           content: contentString,
         });
@@ -63,9 +62,9 @@ export default {
             lat: sensorData.data[index].latitude,
             lng: sensorData.data[index].longitude,
           },
-          label: "sample",
+          label: label,
           map: map,
-          icon: circle,
+          icon: icon,
         });
         marker.addListener("click", () => {
           infowindow.open({
